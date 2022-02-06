765b099ece270839c829d47c0ac2d18f5f642eb1
yellowsub12 <93496177+yellowsub12@users.noreply.github.com>
2022-02-05 22:11:36 -0500

    Python file with several functions
    Python file with several functions that need to be properly put together

--- test.py
+++ test.py
@@ -1,13 +1,49 @@
 import os
+import pwd
 import shutil
 import re
 wacky = str
+#main function
 while wacky != "exit":
     wacky = input()
     if wacky == "exit":
         break
-    wackystring = re.sub(r'->', '>', wacky)
-    os.system(wackystring)
-    print(wackystring)
+
+    if "./" and ".exe" in wacky: 
+        pattern = "./(.*?).exe"
+        substring = re.search(pattern, wacky).group(1)
+        substringA = substring+".exe"
+        print(substringA)
+        shutil.copy(str(shutil.which(substring)), str(pwd) )
+       # os.startfile(shutil.which("notepad.exe"))
+        wackystring = re.sub(r'->', '>', wacky)
+        os.system(wackystring)
+
+    else:
+        wackystring = re.sub(r'->', '>', wacky)
+        os.system(wackystring)
+
+#To find .exe file here, warning, gives too many results, should be integrated to main
+import os, fnmatch
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    print(result)
+    return result
+find('*.exe', '/home/usr/HelloWorld/')
+
+#If you enter ./example.exe, this can root out example and throw example.exe to the filer locator 
+wacky = input()
+if "./" and ".exe" in wacky: 
+        pattern = "./(.*?).exe"
+        substring = re.search(pattern, wacky).group(1)
+        substringA = substring+".exe"
+        print(substringA)
+        print(shutil.which(substringA))
+
+#MISC
 #print(shutil.which("notepad.exe"))
 #os.startfile(shutil.which("notepad.exe"))
