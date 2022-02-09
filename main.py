3ae7eb040fed07974e961b92a266e9276b576623
MatthieuPourrat <50846358+MatthieuPourrat@users.noreply.github.com>
2022-02-09 15:18:04 -0500

    Added Read file and name@host$

--- main.py
+++ main.py
@@ -8,6 +8,27 @@ import fnmatch
 userInput = str
 loopInt=1
 
+def name_host() :
+    open_file = open("file.txt", "r")
+    name = open_file.readline()
+    host = open_file.readline()
+    path = open_file.readline()
+    name = name[:-1]
+    host = host[:-1]
+    path = path[:-1]
+    prompt = name + host
+    return name + "@" + host + "$"
+    open_file.close()
+
+def read_path():
+    open_file = open("file.txt", "r")
+    temp = open_file.readline()
+    temp = open_file.readline()
+    path = open_file.readline()
+    path = path[:-1]
+    return path
+    open_file.close()
+
 #This function finds an .exe file in a given path/directory, warning, may give too many results
 def find(pattern, path):
     result = []
@@ -22,7 +43,8 @@ find('*.exe', '/home/yaqub/HelloWorld/')
 
 #actual loop where commands are given until 'exit' is typed to stop the program
 while loopInt>0:
-    userInput = input()
+    shell_name = name_host()
+    userInput = input(shell_name + " ")
     if userInput == "exit":
         break
 
