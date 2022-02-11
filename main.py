65cd0808c079d8422de85edd23f9c24a8dc53e20
yellowsub12 <93496177+yellowsub12@users.noreply.github.com>
2022-02-11 18:35:21 -0500

    added search for .exe file feature
    Program can now search for .exe file

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#the find(pattern, path) function is a snippet code from StackOverflow
-
 import os
 import shutil
 import re
 import fnmatch
+import pwd
 userInput = str
 loopInt=1
-
 def name_host() :
     open_file = open("file.txt", "r")
     name = open_file.readline()
@@ -25,35 +23,6 @@ def read_path():
     return path
     open_file.close()
 
-def sort_path():
-    string_path = read_path()
-    count = 0
-    for char in string_path:
-        if char == ",":
-            count = count + 1
-    count = count + 1
-    path_array = [0] * count
-    j = 0
-    h = 0
-    i = 0
-    first_char = string_path[0]
-    for letter in string_path:
-        if letter == "," and i == 0:
-            path_array[j] = first_char + string_path[:-1]
-            h = i
-            i = i + 1
-            j = j + 1
-        elif letter == "," :
-            path_array[j] = string_path[h+1:i]
-            h = i
-            i = i + 1
-            j = j + 1
-        else:
-            i = i + 1
-            continue
-
-    path_array[j] = string_path[h+1:i]
-    return str(path_array[2])
 
 
 #This function finds an .exe file in a given path/directory, warning, may give too many results
@@ -65,13 +34,14 @@ def find(pattern, path):
                 result.append(os.path.join(root, name))
     return result
 
+shell_name = name_host()
+path_string = read_path()
+
 
 #actual loop where commands are given until 'exit' is typed to stop the program
 while loopInt>0:
-    shell_name = name_host()
-    os.system("cd " + sort_path())
-    print(read_path()) #to remove for submission. Only here for testing purpose
     userInput = input(shell_name + " ")
+    value = path_string.split(',')
     if userInput == "exit":
         break
 
@@ -80,18 +50,20 @@ while loopInt>0:
         pattern = "(.*?).exe"
         substring = re.search(pattern, userInput).group(1)
         substringA = substring+".exe"
-        if find(substringA, read_path()) == "[]":
-                print("This executable file hasn't been found in the path! Start again!")
-                continue
-        elif find(substringA, read_path()) != "[]":
-                print("Executable file provided found at " + find(substring, read_path()))
-                substringB = find(substringA, read_path()) 
-        userInputstringA = re.sub(r'->', '>', substringB)
-        os.system(userInputstringA)
+        i=0
+        while i<len(value):
+            if find(substringA, value[i]) != []:
+                print("Executable file provided found at " + str(find(substringA, value[i])))
+                os.chdir(value[i])
+                print(os.getcwd())
+                userInputstringA = re.sub(r'->', '>', value[i])
+                os.system(userInputstringA)
+            i+=1        
+ #      userInputstringA = re.sub(r'->', '>', value[i])
+ #      os.system(userInputstringA)
     
 
 
     else:
         userInputstring = re.sub(r'->', '>', userInput)
         os.system(userInputstring)
-
