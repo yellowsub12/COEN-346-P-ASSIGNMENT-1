23a40c9367b6ea4c23424f228fc2b248b74132f4
yellowsub12 <93496177+yellowsub12@users.noreply.github.com>
2022-02-11 23:01:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+#Program written by Ali Turkman (40111059), Mattieu Pourrat (40089209) and Zeineb Ben Mami (40024877) 
+#find() function code snippet from StackOverflow
 import os
 import re
 import fnmatch
 import threading
 
+#first string that designates what will be user string
 userInput = str
+#loop integer to keep while loop going until exit command breaks it
 loopInt=1
 
+#path used to find the file.txt that will be given, integral for both functions below
 absolute_path = os.getcwd()
 
+#reads the file.txt file provided to acquire the host name that will be used in the shell
 def name_host() :
     open_file = open(absolute_path + "/file.txt", "r")
     name = open_file.readline()
@@ -18,6 +24,7 @@ def name_host() :
     return name + "@" + host + "$"
     
 
+#reads the file.txt file provided to acquire the PATH from which many paths are given
 def read_path():
     open_file = open(absolute_path + "/file.txt", "r")
     temp = open_file.readline()
@@ -28,7 +35,7 @@ def read_path():
     return path
     
 
-#This function finds an .exe file in a given path/directory, warning, may give too many results
+#This function finds an .exe file in a given path/directory, searches all subdirectories warning, may give too many results
 def find(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
@@ -37,11 +44,13 @@ def find(pattern, path):
                 result.append(os.path.join(root, name))
     return result
 
+#Detects the word 'cd' to do cd function. Meaning program doesn't call on bash cd function
 def findWholeWord(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
 shell_name = name_host()
 path_string = read_path()
+#the string.split(‘,’) function is used to divide the PATH provided into different usable paths to navigate, and stores every path in an array.
 value = path_string.split(',')
 
 #actual loop where commands are given until 'exit' is typed to stop the program
@@ -49,13 +58,17 @@ while loopInt>0:
     userInput = input(shell_name + " ")
     if userInput == "exit":
         break
+        
 
+#If no path is given, then the program will assume it’s a universal .exe file that can be ran from anywhere (like notepad.exe) and will run it. 
+#If the user wants to rule a local .exe file then they should use ./file.exe as expected with regular terminals.
     if ".exe" in userInput: 
         pattern = "(.*?).exe"
         substring = re.search(pattern, userInput).group(1)
         substringA = substring+".exe"
 
         i=0
+        #It will run it into every PATH provided and will output which path holds the file.exe named. 
         while i<len(value):
             if find(substringA, value[i]) != []:
                 print("Executable file provided found at " + str(find(substringA, value[i])))
@@ -70,6 +83,7 @@ while loopInt>0:
                     t2.start()
                     t2.join()
                 break
+            #If no path is given, then the program will assume it’s a universal .exe file that can be ran from anywhere (like notepad.exe) and will run it. 
             else: 
                 i+=1   
                 if i==len(value): 
@@ -81,13 +95,13 @@ while loopInt>0:
                         t2.start()
                         t2.join()
              
- #      userInputstringA = re.sub(r'->', '>', value[i])
- #      os.system(userInputstringA)
-
+ 
+    #Detects cd input, then navigates to directory there after splitting cd [directory] input
     elif findWholeWord('cd')(userInput) != None:
         userInputstring = re.sub(r'->', '>', userInput)
         os.chdir(userInputstring.split()[1])
     
+   #uses regular expressions to transform -> and ->> into > and >> which are actually bash/command functions
     else:
         userInputstring = re.sub(r'->', '>', userInput)
         if "&" in userInput:
